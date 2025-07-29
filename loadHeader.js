bd173f7d3893b406dd1871d7aa3ba338e6713d91
arif-sk-dev <arifsk.dev@gmail.com>
2025-07-30 01:37:24 +0600

    initial commit 1:37AM

---
+++ loadHeader.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", () => {
+  fetch("header.html")
+    .then(res => res.text())
+    .then(html => {
+      document.getElementById("header-placeholder").innerHTML = html;
+    })
+    .then(() => {
+      const script = document.createElement("script");
+      script.src = "cart.js";
+      document.body.appendChild(script);
+    });
+});
